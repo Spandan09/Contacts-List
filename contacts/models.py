e639b7c6ab3a4ae1ec8cf4542ac478921121e826
Spandan09 <spandan.patnaik@suiit.ac.in>
2021-02-13 20:51:23 +0530

    ...

---
+++ contacts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Contact(models.Model):
+
+    full_name = models.CharField(max_length=255)
+    relationship = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.full_name
